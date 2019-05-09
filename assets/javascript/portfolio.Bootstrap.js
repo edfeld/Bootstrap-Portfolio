10952f0cf1c9539c7cfbf16380c44f2e614ff2db
edfeld <edfeld1@gmail.com>
2019-05-09 11:51:37 -0700

    Update to about me text

--- assets/javascript/portfolio.Bootstrap.js
+++ assets/javascript/portfolio.Bootstrap.js
@@ -1,7 +1,16 @@
 // javascript for Bootstrap portfolio
-
+let first = 'edfeld1';
+let second = 'gmail.com';
+let email;
 $('a.mail').on('click', function(){
     alert("edfeld1@gmail.com");
     var href = $(this).attr('href');
     $(this).attr('href', href.replace('ed@badmail.mydomain.com', 'edfeld1@gmail.com'));
 });
+
+$('.submit-btn').on('click', function (){
+    // alert("Hello");
+    let email = first + "@" + second;
+    console.log("email: ", email);
+    alert("My email is " + email);
+})
